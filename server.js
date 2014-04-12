8e384aedff5ff318fec09867b5c3de78dbcb2211
Scott Daly <scottdaly@Khan.local>
2014-04-12 16:32:10 -0600

    added calls to mongoDB

--- server.js
+++ server.js
@@ -3,6 +3,15 @@
 var express = require('express');
 var fs      = require('fs');
 
+var database = require('./routes/database');
+
+var mongodb = require('mongodb');
+
+var sendError = function(req, res, status, message, closeAndEnd, consoleLogSpecific){
+    console.log(consoleLogSpecific || message);
+    res.send(status, {errorMessage:message});
+    next();
+}
 
 /**
  *  Define the sample application.
@@ -23,7 +32,7 @@ var SampleApp = function() {
     self.setupVariables = function() {
         //  Set the environment variables we need.
         self.ipaddress = process.env.OPENSHIFT_NODEJS_IP;
-        self.port      = process.env.OPENSHIFT_NODEJS_PORT || 8080;
+        self.port      = process.env.OPENSHIFT_NODEJS_PORT || 8085;
 
         if (typeof self.ipaddress === "undefined") {
             //  Log errors on OpenShift but continue w/ 127.0.0.1 - this
@@ -92,32 +101,89 @@ var SampleApp = function() {
     /**
      *  Create the routing table entries + handlers for the application.
      */
+
+    /**
+        Medicine object{ 
+                         id: int
+                         user: string name 
+                         string date: new Date 
+                         repeat: boolean 
+                         days: [ ] 
+                         taken: boolean 
+                        } 
+        Settings object{ 
+                         id: int
+                         user: string 
+                         enableNotifications: boolean 
+                         time: string 
+                         confirmationEmail: string 
+                        }
+    */ 
     self.createRoutes = function() {
         self.routes = { };
 
-        self.routes['getSchedule'] = function(req, res) {
-            res.setHeader('Content-Type', 'application/json');
-            res.send();
+        self.routes['saveMedicine'] = function(req, res, next) {
+            //create the object that will be sent to endpoint
+            var newMedicine = {};   
+
+            var medicinesCollection = req.db.collection('medicines');
+            medicinesCollection.insert(newMedicine, function(err, result){
+                if(err)
+                    sendError(req, res, 500, "error on inserting a new medicine object", false);
+                else{
+                    res.send(200, result);
+                    next();
+                }
+                req.db.close();
+                res.end();
+            });
         };
 
-        self.routes['saveMedicine'] = function(req, res) {
-            res.setHeader('Content-Type', 'application/json');
-            res.send();
+        self.routes['saveSettings'] = function(req, res, next) {
+            var settingsObject = {};   
+
+            var settingsCollection = req.db.collection('settings');
+            settingsCollection.insert(newMedicsettingsObjectine, function(err, result){
+                if(err)
+                    sendError(req, res, 500, "error on inserting a new settings object", false);
+                else{
+                    res.send(200, result);
+                    next();
+                }
+                req.db.close();
+                res.end();
+            });
         };
 
-        self.routes['saveSettings'] = function(req, res) {
-            res.setHeader('Content-Type', 'application/json');
-            res.send();
+        self.routes['getMedicines'] = function(req, res, next) {
+            //get the user
+            var user = "";
+            var medicinesCollection = req.db.collection('medicines');
+            medicinesCollection.find({"user": user}).toArray(function(err, medicines){
+                if(err)
+                    sendError(req, res, 500, "error getting medicines", false);
+                else{
+                    res.json(200, medicines);
+                    next();
+                }
+                req.db.close();
+                res.end();
+            });
         };
 
-        self.routes['getMedicines'] = function(req, res) {
-            res.setHeader('Content-Type', 'application/json');
-            res.send();
-        };
+        self.routes['deleteMedicine'] = function(req, res, next) {
 
-        self.routes['deleteMedicine'] = function(req, res) {
-            res.setHeader('Content-Type', 'application/json');
-            res.send();
+            var medicinesCollection = req.db.collection('medicines');
+            medicinesCollection.remove({}).toArray(function(err, result){
+                if(err)
+                    sendError(req, res, 500, "error deleting medicine", false);
+                else{
+                    res.send(200, result);
+                    next();
+                }
+                req.db.close();
+                res.end();
+            });
         };
     };
 
@@ -128,13 +194,30 @@ var SampleApp = function() {
      */
     self.initializeServer = function() {
         self.createRoutes();
-        self.app = express.createServer();
+        self.app = express();
 
-        self.app.get("/getSchedule", self.routes['getSchedule']);
-        self.app.post("/saveMedicine", self.routes['saveMedicines']);
+        self.app.all('*', function(req, res, next){
+            res.setHeader('Content-Type', 'application/json');
+            // this keeps chrome from crying about cross origin access
+            res.header("Access-Control-Allow-Origin", "*");
+            res.header('Access-Control-Allow-Methods', 'PUT, GET, POST, DELETE, OPTIONS');
+            res.header("Access-Control-Allow-Headers", "Content-Type");
+            if ('OPTIONS' == req.method){
+                console.log('options hit');
+                res.send(200);
+                res.end();
+            }else
+                next();
+        });
+
+        self.app.all('*', database.getDbConnection);// creates db connection and saves it to req.db
+
+        self.app.post("/saveMedicine", self.routes['saveMedicine']);
         self.app.post("/saveSettings", self.routes['saveSettings']);
         self.app.get("/getMedicines", self.routes['getMedicines']);
         self.app.delete("/deleteMedicine", self.routes['deleteMedicine']);
+
+        self.app.all("*", database.closeAll);// does db.close() and res.end()
     };
 
 
@@ -172,4 +255,22 @@ var SampleApp = function() {
 var zapp = new SampleApp();
 zapp.initialize();
 zapp.start();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
